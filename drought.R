5e8512ab77d29fd1a0aa9e1d04c10fd38084d4fa
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-05-19 12:26:19 -0700

    this is the equation that will be used calculate drought mortality. It needs to be calibrated to allow for precipitation and temperature imputs

--- drought.R
+++ drought.R
@@ -1,2 +1,9 @@
 #' Drought Submodel
 #'
+#'
+
+drought_function <- function(SPEI, WD, SLA, SPEI_coeff = -1.28, WD_coeff = 0.38, SLA_coeff = -0.41) {
+  mortality_drought = SPEI_coeff*SPEI + SPEI_coeff*SPEI*WD_coeff*WD + SPEI_coeff*SPEI*SLA_coeff*SLA
+  
+  return(mortality_drought)
+}
