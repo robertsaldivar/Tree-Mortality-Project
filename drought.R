754f9bfe8ad3386fa75aee3966878a695e54e192
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-05-26 13:38:10 -0700

    I have put in a way to calculate the SPEI into the function. I have also converted the results of the function to mortality percent. I am not sure if I have done this correctly so I will need to talk to naomi about it.
    The data I have been using to calculate SPEI is the balance table in the SPEI package.

--- drought.R
+++ drought.R
@@ -1,17 +1,35 @@
 #' Drought Submodel
 #'
 #'
-#'@param SPEI The Standardised Precipitation-Evapotranspiration Index
-#'@param WD This the mean wood density. The default is set for Quercus gambelii
-#'@param SLA The Surface mean Leaf Area. The default is set for Quercus gambelii
+#'@param SPEI The Standardised Precipitation-Evapotranspiration Index The range for the input is -3 to 3
+#'@param WD This the mean wood density. The default is set for Quercus virginiana
+#'@param SLA The Surface mean Leaf Area. The default is set for Quercus virginiana
+#'@return log Mortality (log trees/Trees*Year)
 #'
 #'References for 
 #'
 #' models mortality data (proportion of standing dead trees and annual rates). Models only for adult trees not seedlings or saplings
+#' 
+#'  for the input data I am currently using the balance data from the SPEI package
+
+library(SPEI)
 
 
-drought_function <- function(SPEI, WD = 0.62, SLA = 13.89, SPEI_coeff = -1.28, WD_coeff = 0.38, SLA_coeff = -0.41) {
-  mortality_drought = SPEI_coeff*SPEI + SPEI_coeff*SPEI*WD_coeff*WD + SPEI_coeff*SPEI*SLA_coeff*SLA
+drought_function <- function(input_data, WD = 0.946, SLA = 7.39, coeff1 = -1.28, coeff2 = 0.38, coeff3 = -0.41) {
   
-  return(mortality_drought)
+    
+    SPEI = SPEI::spei(data = input_data, scale = 24)
+    
+    SPEI = as.data.frame(SPEI$fitted)
+    
+    log_mortality_drought = coeff1*SPEI + coeff2*SPEI*WD + coeff3*SPEI*SLA
+    
+    
+    drought_mortality = exp(log_mortality_drought)
+    
+    drought_mortality_rate = drought_mortality/100
+    
+  
+  return(list(drought_mortality_rate))
 }
+
